15cbadce06403ded42d0e15ead6d8f06716174d8
Kdavis4444 <kad2089985@lpssonline.com>
2024-04-04 12:52:50 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,7 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+applyFilter()
   
 
   // do not change the below line of code
@@ -32,6 +32,16 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
+function applyFilter(){
+ for (var i = 0; i < image.length - 1; i++){
+  for(var j = 0; j < image[i].length; j++){
+    image[i][j]
+  }
+ }
+ var rgbString = image[4]
+ rgbStringToArray(rgbString)
+ var rgbNumbers = rgbArrayToString()
+}
 
 
 // TODO 7: Create the applyFilterNoBackground function
