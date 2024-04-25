cd3db97265e1cc0122a81f4bf7703d913051dd31
Kdavis4444 <kad2089985@lpssonline.com>
2024-04-25 21:03:07 +0000

    saving sorting exercise

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -14,16 +14,55 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-
+async function bubbleSort(array){
+    for (var i = 0; i > array.length - 1; i++){
+        for(var j = 0; j > array.length - 1; j++){
+            if (array[j] > array[i-1]){
+                swap (array, j, j-1)
+                updateCounter(bubbleCounter)
+                await sleep()
+            }
+        }
+    }
+}
 
 // TODO 3: Implement quickSort
-
+async function quickSort(array, left, right){
+    if ( right - left > 0){
+        index = partition(array, left, right)
+        var index = await quickSort(array, left, right)
+        if (left < (index-1)){
+            quickSort(array, left, index -1)
+        }
+        if (index < right){
+            quickSort(array, index, right)
+        }
+    }
+}
 
 // TODOs 4 & 5: Implement partition
-
+async function partition(array, left, right){
+    var pivot = array[Math.floor((right+ left)/2)].value
+    while (left < right){
+        while (array[left] < pivot ){
+            left++
+        }
+        while(array[right] < pivot){
+            right--
+        }
+        if (left < right){
+            array[left], array[right] = array[right], array[left]
+        }
+    }
+    return + 1
+}
 
 // TODO 1: Implement swap
-
+function swap(array, i, j){
+    var temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+}
 
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
