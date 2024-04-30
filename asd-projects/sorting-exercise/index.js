15cd63f8e0e749cc50deae0a5ff010e20d65e941
Kdavis4444 <kad2089985@lpssonline.com>
2024-04-30 18:28:25 +0000

    saving sorting exercise

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -15,57 +15,49 @@ The CSS ids you will work with are:
 
 // TODO 2: Implement bubbleSort
 async function bubbleSort(array){
-    for (var i = 0; i > array.length - 1; i++){
-        for(var j = array.length; j >= i + 1; j--){
-            if (array[j].value < array[j-1].value){
-                swap (array, j, j - 1)
-                updateCounter(bubbleCounter)
-                await sleep()
+    for(var i = 0; i <= array.length - 1; i++){
+        for(var j = 0; j <= array.length - 1; j++){
+            if (array[j].value > array[i].value){
+                swap(array, i, j)
+                updateCounter(bubbleCounter);
+                await sleep();
             }
         }
-    }
+    } 
 }
 
 // TODO 3: Implement quickSort
 async function quickSort(array, left, right){
-    if ( right - left > 0){
-        index = partition
-        var index = await quickSort(array, left, right)
-        if (left < index - 1){
-            await quickSort(array, left, index - 1)
+    if((right - left) > 0){
+        index = await partition(array, left, right)
+        if(left < (index - 1)){
+            quickSort(array, left, index - 1)
         }
         if (index < right){
-            await quickSort(array, index, right)
+            quickSort(array, index, right)
         }
     }
 }
 
 // TODOs 4 & 5: Implement partition
+
 async function partition(array, left, right){
-    var pivot = array[Math.floor((right + left)/2)].value
+    var pivot = array[Math.floor((right + left)/2)].value;
     while(left < right){
-        while (array[left].value < pivot){
-            left++
-        }
-        while (array[right].value > pivot){
-            right--
-        }
-        if ( left < right){
-            swap(array, right, left)
-            updateCounter(quickSort)
+        while (array[left].value < pivot) { left++ }
+        while(array[right].value > pivot) { right--}
+        if(left < right){
+            swap(array, left, right)
+            updateCounter(quickCounter)
             await sleep()
         }
     }
     return left + 1
-
 }
-
 
 // TODO 1: Implement swap
 function swap(array, i, j){
-    var temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
+    [array[i], array[j]] = [array[j], array[i]]
     drawSwap(array, i, j)
 }
 
